3ca80fe40a18947e4177fec3840c28b876b0a9dc
Amrit <amritpal@uw.edu>
2025-05-06 22:45:07 -0700

    Added test

---
+++ flutter_frontend/test/title_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tapin/main.dart';
+
+void main() {
+  testWidgets('App loads and shows TapIn@UW title', (WidgetTester tester) async {
+    // Build the app
+    await tester.pumpWidget(const MyApp());
+
+    // Verify the title is displayed in an AppBar (or wherever you render it)
+    expect(find.text('TapIn@UW'), findsOneWidget);
+  });
+}
