fbefbe3495316572f1ca456c2091d247937ada90
yonasnat <yonasnat@uw.edu>
2025-05-26 01:05:11 -0700

    Resolve merge conflict in GeneratedPluginRegistrant.swift

--- flutter_frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -2,8 +2,8 @@
 //  Generated file. Do not edit.
 //
 
+import FlutterMacOS
 import Foundation
-
 
 import cloud_firestore
 import cloud_functions
@@ -12,14 +12,11 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 
-
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
- FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
- FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
- FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
- FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
- FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
- FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
 }
-
-
