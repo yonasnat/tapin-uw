9afca539fee7799f077a94704a36c293312fe53f
yfeng <1714390616@qq.com>
2025-06-04 17:14:40 -0700

    Test CI Fix 2

--- flutter_frontend/test/filter_screen_test.dart
+++ flutter_frontend/test/filter_screen_test.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tapin/filter_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'test_helpers.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+// Fake FirebaseAuth
+class FakeAuth implements FirebaseAuth {
+  @override
+  User? get currentUser => null;
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => null;
+}
+
+// Fake FirebaseFirestore
+class FakeFirestore implements FirebaseFirestore {
+  @override
+  CollectionReference<Map<String, dynamic>> collection(String path) {
+    throw UnimplementedError();
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => null;
+}
 
 void main() {
-  setupFirebaseMocks();
-
   testWidgets('FilterScreen has correct title', (WidgetTester tester) async {
-    // Build our widget
-    await tester.pumpWidget(const MaterialApp(home: FilterScreen()));
+    // Use the fake auth and firestore
+    final fakeAuth = FakeAuth();
+    final fakeFirestore = FakeFirestore();
+
+    await tester.pumpWidget(MaterialApp(
+      home: FilterScreen(auth: fakeAuth, firestore: fakeFirestore),
+    ));
 
     // Find the AppBar and verify its title
     final appBarFinder = find.byType(AppBar);
--- flutter_frontend/test/test_helpers.dart
+++ flutter_frontend/test/test_helpers.dart
@@ -1,16 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-@GenerateMocks([FirebaseAuth, User, FirebaseFirestore, DocumentSnapshot], customMocks: [
-  MockSpec<FirebaseAuth>(as: #MockFirebaseAuthBase),
-  MockSpec<User>(as: #MockUserBase),
-  MockSpec<FirebaseFirestore>(as: #MockFirebaseFirestoreBase),
-  MockSpec<DocumentSnapshot>(as: #MockDocumentSnapshotBase),
-])
-void setupFirebaseMocks() {
+Future<void> setupFirebaseMocks() async {
   TestWidgetsFlutterBinding.ensureInitialized();
-  setupFirebaseCoreMocks();
+  
+  // Create a mock Firebase app with test options
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'test-api-key',
+      appId: 'test-app-id',
+      messagingSenderId: 'test-sender-id',
+      projectId: 'test-project-id',
+    ),
+  );
 } 
